6115cdc67f907d8549a5dffb6d0de2890fa28ac3
DESKTOP-Q2S85GV\Admin <huynhbuingockhoa@gmail.com>
2024-09-08 22:13:27 +0700

    Init for Ex2

--- Exercises/Core/Src/main.c
+++ Exercises/Core/Src/main.c
@@ -92,11 +92,10 @@ int main(void) {
 	/* USER CODE BEGIN WHILE */
 	int cnt = 10;
 	void set_LEDS(GPIO_PinState redstate, GPIO_PinState yellowstate,
-			GPIO_Pinstate greenstate) {
+			GPIO_PinState greenstate) {
 		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, redstate);
 		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, yellowstate);
-		HAL_GPIO_WritePin(LED_, LED_GREEN_Pin, greenstate);
-
+		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, greenstate);
 	}
 	while (1) {
 		switch (cnt) {
@@ -167,10 +166,11 @@ static void MX_GPIO_Init(void) {
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOA, LED_RED_Pin | LED_YELLOW_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, LED_RED_Pin | LED_YELLOW_Pin | LED_GREEN_Pin,
+			GPIO_PIN_RESET);
 
-	/*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
-	GPIO_InitStruct.Pin = LED_RED_Pin | LED_YELLOW_Pin;
+	/*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
+	GPIO_InitStruct.Pin = LED_RED_Pin | LED_YELLOW_Pin | LED_GREEN_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
